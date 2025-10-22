cc621a65d96de29b391c50dcdb63af4368d95bdd
Lalit Pradhan <23f2001893@ds.study.iitm.ac.in>
2025-10-22 17:32:26 +0530

    trying db

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATION=False
 class LocalDevelopmentConfig(Config):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI="sqlite:///secureDb.sqlite3"
+    SQLALCHEMY_DATABASE_URI="postgresql://finai_db_yj7o_user:OQYIxeHizzd7upmgWfl3pAeuk1BVB67p@dpg-d3scecodl3ps73da09cg-a.oregon-postgres.render.com/finai_db_yj7o"
     JWT_SECRET_KEY="this-is-a-secret-key"
