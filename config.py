2570cbd32500731747ddea066755aa4f142de3f2
Lalit Pradhan <23f2001893@ds.study.iitm.ac.in>
2025-12-02 17:19:25 +0530

    h

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATION=False
 class LocalDevelopmentConfig(Config):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI="postgresql://postgres:Lalit@1234@db.nedijqzlukcegqpqgbsg.supabase.co:5432/postgres"
+    SQLALCHEMY_DATABASE_URI="postgresql://postgres:Lalit%401234@db.nedijqzlukcegqpqgbsg.supabase.co:5432/postgres"
     JWT_SECRET_KEY="this-is-a-secret-key"
