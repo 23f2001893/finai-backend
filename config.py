18c7554a84db810dc95f7113a7c8e09dfb7fe774
Lalit Pradhan <23f2001893@ds.study.iitm.ac.in>
2025-12-02 17:37:18 +0530

    hh

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+from urllib.parse import quote
+
 class Config():
     DEBUG=False
     SQLALCHEMY_TRACK_MODIFICATION=False
+
 class LocalDevelopmentConfig(Config):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI="postgresql://postgres:Lalit%401234@db.nedijqzlukcegqpqgbsg.supabase.co:5432/postgres"
+    password = quote("Lalit@1234", safe="")
+    SQLALCHEMY_DATABASE_URI=f"postgresql://postgres:{password}@db.nedijqzlukcegqpqgbsg.supabase.co:5432/postgres"
     JWT_SECRET_KEY="this-is-a-secret-key"
